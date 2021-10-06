706d4bbedb0c1afeec7b55e682db48891268f05d
Tomasz Wrobel <tomasz.wrobel@miquido.com>
2021-10-06 15:23:51 +0200

    Added first test

---
+++ src/main/java/pl/javastart/restassured/test/FirstRestAssuredAutomatedTest.java
@@ -0,0 +1,16 @@
+package pl.javastart.restassured.test;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class FirstRestAssuredAutomatedTest {
+
+    @Test
+    public void givenNonExistingPetIdWhenGetPetThenPetNotFoundTest() {
+        given().when()
+                .get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/0")
+                .then()
+                .statusCode(404);
+    }
+}
