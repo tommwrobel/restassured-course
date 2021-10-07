17867fd4777e547542bc47ed054c0a01c5f293f7
Tomasz Wróbel <tomm.wrobel@gmail.com>
2021-10-08 00:55:43 +0200

    Added delete mmethod test and gson dependency

--- src/main/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
+++ src/main/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
@@ -117,4 +117,50 @@ public class BasicHttpMethodsTests {
                 .log().all()
                 .statusCode(200);
     }
+
+    @Test
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
+
+        String pet = """
+                {
+                  "id": 345,
+                  "category": { 
+                    "id": 1, 
+                    "name": "dogs" 
+                  }, 
+                  "name": "Czarek", 
+                  "photoUrls": [ 
+                    "http://photos.com/dog1.jpg" 
+                  ], 
+                  "tags": [ 
+                    { 
+                      "id": 1, 
+                      "name": "dogs-category" 
+                    } 
+                  ], 
+                  "status": "available" 
+                }
+                """;
+
+        given()
+            .log().all()
+            .body(pet)
+            .contentType("application/json")
+        .when()
+            .post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+        .then()
+            .log().all()
+            .statusCode(200);
+
+        given()
+            .log().all()
+            .contentType("application/json")
+            .pathParam("petId", 345)
+        .when()
+            .delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+        .then()
+            .log().all()
+            .statusCode(200);
+    }
+
 }
